1a76b4852b96d08db4444b5e786fc89c27d7c5e6
Braian Mena <braianmena@gmail.com>
2024-03-17 20:41:56 -0300

    Tarea de clase 39

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -11,15 +11,56 @@ export const getAllProducts = async (req, res) => {
 
 export const getProductById = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
+  console.log(req.params, "params");
+
+  if (productos.length === 0) return  res.status(204).send("No existen productos para buscar");
+
+
   const productFound = productos.find((product) => product.id === id);
 
   if (productFound) {
     return res.status(200).json(productFound);
   }
 
-  res.status(404).json( {message: "No hemos podido encontrar el producto solicitado."} );
+  res.status(404).json({ message: "No hemos podido encontrar el producto solicitado." });
 
   //   res.status(200).json(productos.filter((p) => p.id === id));
-  console.log(productFound);
+  //console.log(productFound);
+};
+
+// TAREA
+// •	Crear endpoint getProductByCategory (ruta y controlador) y debe devolver los productos con la categoría recibida por parámetro.
+export const getProductsByCategory = async (req, res) => {
+  const { category } = req.params;
+    console.log(category, "<-- categoria");
+
+    if (productos.length === 0) return  res.status(204).send("No existen productos para ordenar");
+
+
+    const productsByCategory = productos.filter( product => product.categoria.toLowerCase() === category.toLowerCase());
+    console.log(productsByCategory,"<-agarrados");
+
+    productsByCategory.length !== 0 ? (
+            res.status(200).json(productsByCategory)
+        ) : (
+            res.status(404).json({ message: "No hemos podido encontrar la cotegoria solicitada"})
+    );
+};
+
+// •	Crear el endpoint sortProductByPrice que debe recibir un parámetro que puede adoptar los valores “asc” o “desc” y debe devolver los productos ordenados por precio.
+export const sortProductsBryPrice = async (req, res) => {
+    const { sort } = req.params;
+    console.log(sort,"<--orden");
+
+    if (productos.length === 0) return  res.status(204).send("No existen productos para ordenar");
+
+    if(sort === "asc"){
+        const asc = productos.sort( (a, b) => a.precio - b.precio);
+        res.status(200).json(asc);
+    }else if(sort === "desc"){
+        const desc = productos.sort( (a, b) => b.precio - a.precio);
+        res.status(200).json(desc);
+    }else{
+        res.status(404).json({message: "No se puede ordenar de dicha manera."});
+    }
 };
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { getAllProducts, getProductById } from "../controllers/productos.controllers.js";
+import { getAllProducts, getProductById, getProductsByCategory, sortProductsBryPrice } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 // Cuando pido la URL, se ejecuta el controlador y devuelve el json con los productos
 router.get("/productos", getAllProducts);
 router.get("/producto/:id", getProductById); // Declaramos que se va a recibir una variable de nombre 'id' y mediante 'value' en postman le enviamos el valor
+
+router.get("/productos/:category", getProductsByCategory); // ¿? Preguntar 'por qué no funciona con "/producto", y en dicho caso se ejecuta el endpoint de arriba.
+router.get("/productos/sort/:sort", sortProductsBryPrice);
 
 export default router;
